7e55e1b3c3cc1b61621c0bb2e982c97fe42a5ac2
Ahmed Radwan <marimradwan922@gmail.com>
2025-09-01 17:50:50 +0300

    index.sql

---
+++ index.sql
@@ -0,0 +1,20 @@
+--  Index  --> 1) clustered Index   --> 2) non clustered index  --> unique Index 
+  
+      --> Non clustered Index  = --> unique index 
+
+-- How to create Clustered Index
+
+
+-- create clustered index + name_of_your_clustered_Index + on + table_name(object_name) 
+
+
+create table m (id int , name varchar(10))
+
+
+create clustered index clu_pk on m (id)
+
+-- non clustered index --> Helping in search it is fast search in our system\
+
+create nonclustered index non_pk on dbo.m(name)
+
+-- often clustered or non clustered helps our to fast select from Database
