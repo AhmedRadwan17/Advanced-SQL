5a1e3f249faa18fe2d6b62251e3f3f65e30fc65e
Ahmed Radwan <marimradwan922@gmail.com>
2025-09-04 19:02:41 +0300

    Check option.sql

--- Check option.sql
+++ Check option.sql
@@ -1,10 +1,12 @@
 
--- check option
+-- check option --> it helps to provide your seaurity and make insert into more restricted by the condition 
 
 create view v6 as select  St_Id, st_fname , St_Lname , st_address from Student where St_Address in ('Mansoura' , 'Cairo') with check option
 
 select * from v6
 
 insert into v6 (st_id,st_fname,St_Lname,st_address ) values (210,'samy' ,'ahmed' ,'Mansoura')
-
+  
+--ERROR AS WE ENTER DATA OUT OF THE RANGE
 insert into v6 (st_id,st_fname,St_Lname,st_address ) values (140,'samy' ,'ahmed' ,'asaaasas')
+
